ae0fbbc8889313f59def8781b6a487db94e2613d
Ben Silva <berna.loyola@gmail.com>
2016-05-04 16:47:28 -0700

    added bootstrap

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -3,10 +3,9 @@ $(function (){
 	var resultList = $('#results');
 	var weatherInfo = $('#weather-info');
 
-	$('#city-query').on('submit', function(e){
+	$('#city-query').on('submit keyup', function(e){
 		e.preventDefault();
-		resultList.empty();
-		weatherInfo.empty();
+		
 		var query = $(this).find('input').val();
 		$.ajax({
 			url: 'http://autocomplete.wunderground.com/aq?query=' + query,
@@ -14,28 +13,43 @@ $(function (){
 			dataType: 'jsonp',
 			jsonp: 'cb',
 			success: function(search){
-				search.RESULTS.forEach(function(city){
-					$('<li>')
-						.addClass('search-result')
-						.attr('data-link', city.l)
-						.text(city.name)
-						.appendTo(resultList);
+				console.log(search);
+				resultList.empty();
+				var results = search.RESULTS.filter(function(result){
+					return result.type === "city";
 				});
+				for (var i=0; i<5; i++){
+					result = results[i];
+					if (result.type === "city") {
+						$('<a>').addClass('search-result').addClass('list-group-item').attr('data-link', result.l).text(result.name).appendTo(resultList);
+					}
+				}
 			}
 		});
 	});
 
 	$('#results').on('click', '.search-result', function(){
-		resultList.empty();
+		// resultList.empty();
 		weatherInfo.empty();
+		resultList.find('.active').removeClass('active');
 		var city = $(this);
+		city.addClass('active');
 		$.ajax({
-			url: 'http://api.wunderground.com/api/' + weatherKey + '/forecast/' + city.data('link') + ".json",
+			url: 'http://api.wunderground.com/api/' + weatherKey + '/geolookup/conditions/forecast' + city.data('link') + ".json",
 			method: 'get',
 			dataType: 'jsonp',
-			success: function(city){
-				console.log(city);
-				$('<article>').text(city.forecast.simpleforecast.forecastday[0].conditions).appendTo(weatherInfo);
+			success: function(response){
+				console.log(response);
+				if (response.response.error){
+					$('<article>').append($('<h4>').text("Please select a city to view forecast")).appendTo(weatherInfo);
+				} else {
+					$('<article>')
+						.append($('<h4>').text(response.location.city + ", " + response.location.country_name))
+						.append($('<p>').text(response.current_observation.temp_c + " degrees Celsius"))
+						.append($('<p>').text(response.current_observation.weather))
+						.append($('<img>').attr('src', response.current_observation.icon_url))
+						.appendTo(weatherInfo);
+				}
 			}
 		});
 	});
